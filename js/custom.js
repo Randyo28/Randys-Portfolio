e2252e9f86e5c7187d6e966c1d41b4d28dcbf8ca
Randy Ortiz <Randyortiz28@gmail.com>
2020-10-09 00:33:08 -0400

    added gsap

--- js/custom.js
+++ js/custom.js
@@ -9,12 +9,75 @@
 
 /*----------- CUSTOM JS SCRIPTS -----------*/
 
-(function($) {
-  'use strict';
-  $(function() {
-    // Code here executes When the DOM is loaded...
-  });
-  $(window).on('load', function() {
-    // Code here executes When the page is loaded
-  });
-}(jQuery));
+// (function($) {
+//   'use strict';
+//   $(function() {
+//     // Code here executes When the DOM is loaded...
+//   });
+//   $(window).on('load', function() {
+//     // Code here executes When the page is loaded
+//
+//   });
+// }(jQuery));
+
+
+
+
+
+  //Gsap
+var tl = gsap.timeline();
+
+tl.fromTo('.navbar-nav .nav-item',{
+  x: 200,
+  opacity:0,
+},{
+  delay:1,
+  duration: 1,
+  x:0,
+  opacity:1,
+});
+
+tl.fromTo('.navbar-brand',{
+  x: -150,
+  opacity:0,
+},{
+  opacity:1,
+  duration: 1.2,
+  x:0,
+},
+'>-1.7'
+);
+
+tl.fromTo('.social-icons .icon',{
+  y: 50,
+  opacity:0,
+},{
+  duration: 0.5,
+  y:0,
+  opacity:1,
+  stagger:0.2,
+},
+'>-1'
+);
+
+
+
+    //Scroll-triggers
+// ScrollTrigger.defaults({
+//     markers:true
+//   });
+
+// tl.fromTo("", {
+//   x:'100%',
+//   ease: "none",
+// }, {
+//   x:0,
+//   scrollTrigger: {
+//     trigger: '#main-services',
+//     // toggleActions: 'play reverse',
+//     start: 'top bottom',
+//     end: 'center center',
+//      scrub: 1,
+//     id:"",
+//   }
+// });
