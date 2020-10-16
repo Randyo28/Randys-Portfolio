8f644cc4d5491490c477c8cd3cb1a1e84dcefe9f
Randy Ortiz <Randyortiz28@gmail.com>
2020-10-16 00:40:26 -0400

    compressed images

--- js/custom.js
+++ js/custom.js
@@ -8,19 +8,6 @@
  * License: Themeforest Standard Licenses: https://themeforest.net/licenses
  */
 
-/*----------- CUSTOM JS SCRIPTS -----------*/
-
-// (function($) {
-//   'use strict';
-//   $(function() {
-//     // Code here executes When the DOM is loaded...
-//   });
-//   $(window).on('load', function() {
-//     // Code here executes When the page is loaded
-//
-//   });
-// }(jQuery));
-
         //Convert pdf to
 //https://cloudconvert.com/pdf-to-html
 
